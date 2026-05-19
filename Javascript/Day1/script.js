872667fdca9f01e07a4f5de54995ffd0269644c8
anveshnandre <anveshnandre@gmail.com>
2026-05-19 11:26:19 +0530

    added

---
+++ Javascript/Day1/script.js
@@ -0,0 +1,113 @@
+//console.log("Hello World");
+// var num1 = 10;
+// var num2 = 20;
+// if (num1 > num2){
+//     console.log(num1+"is greater than"+num2)
+// }else {
+//     console.log(num2+"is less than"+num1)
+// }
+// console.log("Odd or Even program")
+// var n=10;
+// if (n%2==0){
+//     console.log(n + " is a EVEN number");
+// }else{
+//     console.log(n + " is a ODD number");
+// }
+// var n=3;
+// var c=0;
+// for(var i=2; i<=Math.sqrt(n); i++) {
+//     if (n%i==0) {
+//         c+=1;
+//     }
+// }
+// if (c==0 & n>1) {
+//     console.log(n + "is prime");
+// } else {
+//     console.log(n + "is not prime");
+// }
+
+
+// var num = 151;
+// var s=0
+// while (num>0) {
+//     r=num%10;
+//     s=s*10+r;
+//     num=Math.floor(num/10);
+// }
+// if 
+// console.log(num+ "is palindrome")
+
+
+// console.log(myFun(1,1));
+// function myFun(num1, num2) {
+//     return num1 - num2
+// }
+// function myFun(num1, num2) {
+//     return m=num1+num2
+// }
+
+
+// function myFun() {
+// }
+// const fun = function(num1 , num2) {
+//     return num1 + num2
+// }
+// console.log(fun(1,1));
+
+
+// const fun = (num1, num2) => {
+//     return num1 + num2
+// }
+// console.log(fun(3,2))
+
+
+// const fun = (num1, num2) => num1 + num2
+// console.log(fun(2,2))
+// let arr = [10,412.123, 'hello', true]
+// console.log(arr)
+
+
+// // arr.push(30)
+// // arr.pop()
+// // arr.unshift()
+// // arr.shift()
+// console.log(arr.length)
+
+
+// let obj = {
+//     id: 1,
+//     name: "Anvesh",
+//     age: 23,
+//     salary: 500000,
+//     techstact: ["python","html","css"]
+// }
+// console.log(obj.name);
+// console.log(obj.salary);
+// console.log(obj.techstact[2]);
+
+// let data = [
+//     {
+//         id=1,
+//         name:"shiva",
+//         age:25,
+//         techstact:["C", "PYTHON", "HTML"]
+//     },
+//     {
+//         id:2,
+//         name="teja",
+//         age:23,
+//         techstact:["Java","CSS"]
+//     }
+// ]
+// console.log(data[1].name); 
+
+var arr = ["Anvesh", "Hari", "charan", "abhi", "Shiva"];
+
+// for (var i = 0; i < 4; i++) 
+
+//     console.log(arr[i]);
+       
+       
+        arr.pop([0])
+        console.log(arr)
+        
---
+++ Javascript/Day2/script.js
@@ -0,0 +1,15 @@
+
+let bob = document.getElementsByClassName("bob")
+console.log(bob[0]);
+
+let hi=document.getElementById("hi")
+hi.innerHTML="<u>Iam Anvesh</u>"
+
+let bobs =document.getElementsByClassName("bob")[0]
+bobs.innerHTML="<u>CSS</u>"
+
+// document.getElementsByClassName("bob")[0].innerHTML= "HTML CSS"
+
+let bobi=document.getElementsByTagName("p")
+bobi[1].innerHTML= "HELLOO"
+
